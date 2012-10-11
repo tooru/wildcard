4993369e6631bdd445de47daa2ffae3d05e21442
高橋 透 <tooru@MacBookPro.local>
2012-10-12 00:25:18 +0900

    Initial commit

---
+++ src/main/java/tooru/wildcard/App.java
@@ -0,0 +1,13 @@
+package tooru.wildcard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
